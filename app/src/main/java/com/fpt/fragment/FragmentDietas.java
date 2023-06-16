c5da52c21446cb151707b66eda81bb663e56dbac
MiguelAngelSilva0108 <miguel.a_silva@outlook.com>
2023-06-16 11:54:44 -0600

    Los elementos de comida han quedado correctamente, así mismo, se implementó una nueva parte para poner para déficit calórico

--- app/src/main/java/com/fpt/fragment/FragmentDietas.java
+++ app/src/main/java/com/fpt/fragment/FragmentDietas.java
@@ -69,32 +69,82 @@ public class FragmentDietas extends Fragment {
     private void initData() {
 
         versionsList = new ArrayList<>();
+        //DESAYUNO
+        versionsList.add(new VersionsDieta("Desayunos ", "Algo Ligero", "Défict calórico",
+                        "\n•Desayuno 1\n" +
+                        "   - Avena\n" +
+                        "   - Un mix de frutos secos\n" +
+                                "-Freas o plátanos\n\n" +
+                                "•Desayuno 2\n"+
+                                "   - Yogurt Natural\n" +
+                                "   - Frutas Frescas\n" +
+                                "   - 1 Cda de salvado de avena, semillas de linaza o chia\n\n"+
+                                "•Desayuno 3\n"+
+                                "   - Juego de frutas con 1 cdta de ajonjolí o semillas de linaza\n" +
+                                "   - Una tortilla de verduras variadas\n\n" ,R.drawable.deficit,
+                "Algo pesado", "SuperHábit Calórico",
+                "\n•Desayuno 1\n" +
+                        "   - Avena\n" +
+                        "   - Un mix de frutos secos\n" +
+                        "-Freas o plátanos\n\n" +
+                        "•Desayuno 2\n"+
+                        "   - Yogurt Natural\n" +
+                        "   - Frutas Frescas\n" +
+                        "   - 1 Cda de salvado de avena, semillas de linaza o chia\n\n"+
+                        "•Desayuno 3\n"+
+                        "   - Juego de frutas con 1 cdta de ajonjolí o semillas de linaza\n" +
+                        "   - Una tortilla de verduras variadas\n\n", R.drawable.calorias));
+//COMIDA
+        versionsList.add(new VersionsDieta("Comida", "Algo Ligero", "Défictcalórico",
+                "\n•Desayuno 1\n" +
+                        "   - Avena\n" +
+                        "   - Un mix de frutos secos\n" +
+                        "-Freas o plátanos\n\n" +
+                        "•Desayuno 2\n"+
+                        "   - Yogurt Natural\n" +
+                        "   - Frutas Frescas\n" +
+                        "   - 1 Cda de salvado de avena, semillas de linaza o chia\n\n"+
+                        "•Desayuno 3\n"+
+                        "   - Juego de frutas con 1 cdta de ajonjolí o semillas de linaza\n" +
+                        "   - Una tortilla de verduras variadas\n\n" ,R.drawable.deficit,
+                "Algo pesado", "SuperHábit Calórico",
+                "\n•Desayuno 1\n" +
+                        "   - Avena\n" +
+                        "   - Un mix de frutos secos\n" +
+                        "-Freas o plátanos\n\n" +
+                        "•Desayuno 2\n"+
+                        "   - Yogurt Natural\n" +
+                        "   - Frutas Frescas\n" +
+                        "   - 1 Cda de salvado de avena, semillas de linaza o chia\n\n"+
+                        "•Desayuno 3\n"+
+                        "   - Juego de frutas con 1 cdta de ajonjolí o semillas de linaza\n" +
+                        "   - Una tortilla de verduras variadas\n\n", R.drawable.calorias));
+        //CENA
+        versionsList.add(new VersionsDieta("Cena", "Algo Ligero", "Défictcalórico",
+                "\n•Desayuno 1\n" +
+                        "   - Avena\n" +
+                        "   - Un mix de frutos secos\n" +
+                        "-Freas o plátanos\n\n" +
+                        "•Desayuno 2\n"+
+                        "   - Yogurt Natural\n" +
+                        "   - Frutas Frescas\n" +
+                        "   - 1 Cda de salvado de avena, semillas de linaza o chia\n\n"+
+                        "•Desayuno 3\n"+
+                        "   - Juego de frutas con 1 cdta de ajonjolí o semillas de linaza\n" +
+                        "   - Una tortilla de verduras variadas\n\n" ,R.drawable.deficit,
+                "Algo pesado", "SuperHábit Calórico",
+                "\n•Desayuno 1\n" +
+                        "   - Avena\n" +
+                        "   - Un mix de frutos secos\n" +
+                        "-Freas o plátanos\n\n" +
+                        "•Desayuno 2\n"+
+                        "   - Yogurt Natural\n" +
+                        "   - Frutas Frescas\n" +
+                        "   - 1 Cda de salvado de avena, semillas de linaza o chia\n\n"+
+                        "•Desayuno 3\n"+
+                        "   - Juego de frutas con 1 cdta de ajonjolí o semillas de linaza\n" +
+                        "   - Una tortilla de verduras variadas\n\n", R.drawable.calorias));
 
-        versionsList.add(new VersionsDieta("Pierna", "Primer entrenamiento", "Principiantes",
-                "• Realiza un breve calentamiento con peso muerto por 3 series de 40 repeticiones, ligeras.\n\n" +
-                        "• Primer set:\n" +
-                        "   - Peso muerto olímpico 10 reps x 4 series\n" +
-                        "   - Peso muerto a una pierna con mancuerna 20reps x 4 series\n\n" +
-                        "• Segundo set:\n" +
-                        "   - Sentadilla profunda x 12 reps x 4 series\n" +
-                        "   - Prensa pies arriba juntos 15 reps x 4 series", R.drawable.pierna,
-                "texto", "texto", "• Realiza un breve calentamiento con peso muerto por 3 series de 40 repeticiones, ligeras.\n\n" +
-                "• Primer set:\n" +
-                "   - Peso muerto olímpico 10 reps x 4 series\n" +
-                "   - Peso muerto a una pierna con mancuerna 20reps x 4 series\n\n" +
-                "• Segundo set:\n" +
-                "   - Sentadilla profunda x 12 reps x 4 series\n" +
-                "   - Prensa pies arriba juntos 15 reps x 4 series", R.drawable.cuadricep));
 
-        versionsList.add(new VersionsDieta("Brazo", "1", "1.0", "Primer set: Curl de bíceps barra Z 15rep x 4 series, curl barra romana, Segundo set: Curl araña más curl invertido 15rep x 4 series",
-                R.drawable.cuadricep, "", "", "", R.drawable.cuadricep));
-        versionsList.add(new VersionsDieta("Pecho", "1", "1.0", "Primer set: Press con mancuerna en banco horizontal 15rep x 4 series más press máquina más pull over 15reps x 4 series, Segundo set: Press inclinado usando mancuernas 15reps x 4 series, Cristos banco plano 15reps x 4 series",
-                R.drawable.cuadricep, "", "", "", R.drawable.cuadricep));
-        versionsList.add(new VersionsDieta("Espalda", "1", "1.0", "Jalon al frente abierto más remo mancuerna 15reps x 4 series, Remo con barra más jalón individual polea alta 15reps x 4 series, Hiper-extensión más dominadas al fallo",
-                R.drawable.cuadricep, "", "", "", R.drawable.cuadricep));
-        versionsList.add(new VersionsDieta("Hombro", "1", "1.0", "Press de hombro con barra más laterales con mancuerna 15reps x 4 series, Press con mancuerna con pájaros acostado más a una mano con mancuernas (pajaro) 15reps x 4 series, Press a una mano de pie más frontal con mancuerna 15reps x 4 series",
-                R.drawable.cuadricep, "", "", "", R.drawable.cuadricep));
-        versionsList.add(new VersionsDieta("Tricep", "1", "1.0", "Extensión con barra polea alta más copa a dos manos con mancuernas separadas, Press francés prexhaustivo (de 1 a 5 más 5 dobles) más patadas de trícep",
-                R.drawable.cuadricep, "", "", "", R.drawable.cuadricep));
     }
 }
